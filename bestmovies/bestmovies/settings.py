e05dbd358b98daed361d1b1fe5f61e1b92801bbf
RomanMRR <roman.mukhametov.r@gmail.com>
2022-12-27 18:08:58 +0300

    Изменено место сохранения файла csv

--- bestmovies/bestmovies/settings.py
+++ bestmovies/bestmovies/settings.py
@@ -17,7 +17,7 @@ SPIDER_MODULES = ['bestmovies.spiders']
 NEWSPIDER_MODULE = 'bestmovies.spiders'
 
 FEEDS = {
-    'data/%(name)s/%(name)s.csv': {
+    'data/%(name)s.csv': {
         'format': 'csv',
         'encoding': 'utf8',
         'store_empty': False,
